c6c59b71d0c163a0793c11fa90a8c1f0d37fba9b
JeanPaulSidi <paulsidi@outlook.com>
2025-01-15 20:26:13 -0800

    Added DisplayMenu Method

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,6 +6,11 @@
         {
             return 5;
         }
+
+        static void DisplayMenu() 
+        {
+            Console.WriteLine("************* Menu ***********");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
